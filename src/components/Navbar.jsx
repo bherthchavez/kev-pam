95b82924e31ba1b7f326a9ade07e4cb88d178cfa
bherth <bherthchavez@gmail.com>
2024-03-29 22:27:38 +0800

    navbar adjustment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
 
     return (
         <>
-            <div className={`z-20 bg-[#000000] font-glacial tracking-widest font-bold text-lg  fixed flex justify-between px-4 sm:px-8 items-center top-0 w-screen h-20 text-white`}>
+            <div className={`z-20 bg-[#000000] font-glacial tracking-widest font-bold text-[1.4rem]  fixed flex justify-between px-8 sm:px-20 md:px-42 lg:px-52 items-center top-0 w-screen h-36 text-white`}>
                 <a href='/' className="cursor-pointer">
-                    <h1 className=''> kev+pam</h1>
+                    <h1 className='text-[1.2rem]'> kev+pam</h1>
                 </a>
-                <ul className="hidden md:flex gap-8 ">
+                <ul className="hidden md:flex  gap-8 ">
                     <Link to='simbahan' smooth={true} duration={500}>
                         <li className='p-2 group transition-all duration-300 ease-in-out cursor-pointer'>
                             <span className='w-full text-md text-[#fef1e2] cursor-pointer bg-left-bottom bg-gradient-to-r from-[#fef1e2] to-[#fef1e2] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
