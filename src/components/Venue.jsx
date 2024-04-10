365fe363ea3075b77c58ec9875283333e9cc7748
bherth <bherthchavez@gmail.com>
2024-04-10 21:25:32 +0800

    adding kailan

--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import saan from '../assets/saan.jpg'
+import kailan from '../assets/kailan.png'
 
 import ReactCurvedText from 'react-curved-text';
 
 const Venue = () => {
     return (
         <>
-            <div name="kailan" className='max-w-[1000px] mx-auto pt-16 pb-32'>
-                <div className='flex flex-col sm:flex-row gap-4 justify-around items-center px-10 sm:px-15 md:px-20 lg:px-42 xl:px-0 2xl:px-0'>
-                    <div className='flex flex-col gap-5 text-white text-center text-base sm:text-2xl'>
+            <div name="kailan" className='w-full'>
+                <div className='flex flex-col sm:flex-row gap-4 justify-between items-center '>
+                    <div className='flex  flex-col w-full sm:w-[30%] justify-center items-center text-[#EBE7E4] text-center text-base sm:text-xl'>
                       
-                        <div className='flex sm:hidden'>
+                        {/* <div className='flex sm:hidden'>
                             <ReactCurvedText width='321'
                                 height='100'
                                 cx='163'
@@ -25,10 +26,9 @@ const Venue = () => {
                                 tspanProps={{ "dy": "6" }}
                                 ellipseProps={null}
                                 svgProps={{ "style": { "transform": "rotate(0deg)" } }} />
-                        </div>
-
-                        <div className='hidden sm:flex'>
-                            <ReactCurvedText width='321'
+                        </div> */}
+                        <div className=''>
+                            {/* <ReactCurvedText width='321'
                                 height='140'
                                 cx='163'
                                 cy='156'
@@ -41,25 +41,25 @@ const Venue = () => {
                                 textPathProps={{ "fill": "#ffffff" }}
                                 tspanProps={{ "dy": "6" }}
                                 ellipseProps={null}
-                                svgProps={{ "style": { "transform": "rotate(0deg)" } }} />
+                                svgProps={{ "style": { "transform": "rotate(0deg)" } }} /> */}
+                                <img src={kailan} className='w-56 sm:w-80' />
+
                         </div>
-
+                    
                         
-
-
-
-                        <h1> june 8, 2024, saturday <br></br> 3:00 PM</h1>
+                        <h1 className='font-extrabold mb-5'> saturday, june 08, 2024 <br></br> 3:30 in the afternoon</h1>
                         <a href='https://maps.app.goo.gl/wEwkVMmAmMhBcFVE7'
                             target='_blank' rel='noreferrer noopener'
-                            className='underline'>Savanna Farm Tagaytay  <br></br>Alfonso, Cavite</a>
+                            className='underline font-extrabold mb-5'>SAVANNA FARM TAGAYTAY  <br></br>
+                            <p className='font-thin text-lg'>Sicat Road, Alfonso, Cavite</p>
+                            </a>
                     </div>
 
-                    <div className=''>
-                        <img src={saan} className=' rounded-xl border-[#403F10] border-2' />
+                    <div className='w-full sm:w-[70%]'>
+                        <img src={saan} className='' />
                     </div>
                 </div>
             </div>
-
             <div className={'p-4 lg:text-base flex justify-center text-slate-700'}>
           <a href='https://julbertpruel.netlify.app/' className='text-xs'>
             ⓒ {new Date().getFullYear()} Julbert Pruel
