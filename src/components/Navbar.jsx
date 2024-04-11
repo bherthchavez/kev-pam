9149e56c8c069dd45531d04b3a8f8e51472aa2fa
bherth <bherthchavez@gmail.com>
2024-04-11 16:25:08 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,22 +20,13 @@ const Navbar = () => {
         scroll.scrollToBottom(1000);
       };
     
-      const scrollToEntourage = () => {
-        scroll.scrollTo(1000); // Scrolling to 100px from the top of the page.
+      const scrollToEntourage = (px) => {
+        scroll.scrollTo(px); // Scrolling to 100px from the top of the page.
       };
-      const scrollToEntourageBS = () => {
-        scroll.scrollTo(1550); // Scrolling to 100px from the top of the page.
-      };
-      const scrollToKailan = () => {
-        scroll.scrollTo(250); // Scrolling to 100px from the top of the page.
-      };
-      const scrollToKailanBS = () => {
-        scroll.scrollTo(760); // Scrolling to 100px from the top of the page.
+      const scrollToKailan = (px) => {
+        scroll.scrollTo(px); // Scrolling to 100px from the top of the page.
       };
     
-      const scrollMore = () => {
-        scroll.scrollMore(-700); // Scrolling an additional 100px from the current scroll position.
-      };
 
 
     return (
@@ -45,14 +36,14 @@ const Navbar = () => {
                     <h1 className='text-[1.5rem] '> kev+pam</h1>
                 </a>
                 <ul className="hidden md:flex  rounded gap-8 text-sm sm:text-lg md:text-xl lg:text-2xl font-medium text-[#EBE7E4]">
-                    <nav onClick={scrollToKailanBS}>
+                    <nav onClick={()=>scrollToKailan(760)}>
                         <li className='p-2 group transition-all duration-300 ease-in-out cursor-pointer'>
                             <span className='w-full  cursor-pointer bg-left-bottom bg-gradient-to-r from-[#fef1e2] to-[#fef1e2] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
                                 kailan at saan?
                             </span>
                         </li>
                     </nav>
-                    <nav onClick={scrollToEntourageBS}>
+                    <nav onClick={()=>scrollToEntourage(1550)}>
                         <li className=' p-2 group transition-all duration-300 ease-in-out cursor-pointer'>
                             <span className='w-full cursor-pointer bg-left-bottom bg-gradient-to-r from-[#fef1e2] to-[#fef1e2] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
                                 entourage
@@ -103,12 +94,12 @@ const Navbar = () => {
                 <div className='flex flex-col justify-center gap-32 items-center  h-screen '>
 
                     <ul className='text-3xl flex flex-col  gap-8 '>
-                        <nav onClick={scrollToKailan} >
+                        <nav onClick={()=>scrollToKailan(250)} >
                             <li onClick={handleNav} className=' cursor-pointer '>
                                 kailan at saan?
                             </li>
                         </nav>
-                        <nav onClick={scrollToEntourage} >
+                        <nav onClick={()=>scrollToEntourage(1000)} >
                             <li onClick={handleNav} className=' cursor-pointer '>
                                 entourage
                             </li>
